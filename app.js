81d539fa859b0fc3c1af235758bebd642039a18d
iamjunho@gmail.com <iamjunho@gmail.com>
2021-02-19 19:01:14 +0900

    Lecture 2.2 - Recap

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 'use strict';
 
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = '#2c2c2c';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
+  if (!painting) {
+    console.log('creating path in', x, y);
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    console.log('creating line in', x, y);
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-function onMouseDown(event) {
+function onMouseDown() {
   painting = true;
 }
 
@@ -17,14 +33,18 @@ function onMouseUp(event) {
   stopPainting();
 }
 
-function stopPainting(event) {
+function stopPainting() {
   painting = false;
+}
+
+function startPainting() {
+  painting = true;
 }
 
 if (canvas) {
   canvas.addEventListener('mousemove', onMouseMove);
-  canvas.addEventListener('mousedown', onMouseDown);
-  canvas.addEventListener('mouseup', onMouseUp);
+  canvas.addEventListener('mousedown', startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
   canvas.addEventListener('mouseleave', stopPainting);
 }
 
